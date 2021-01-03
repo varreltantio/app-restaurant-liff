b7b4a14d89c84a9cd0fa8c478c79d9cf7a5511ff
varreltantio <snetid90@gmail.com>
2021-01-03 16:18:52 +0700

    add loading app

--- js/liff-starter.js
+++ js/liff-starter.js
@@ -69,10 +69,12 @@ function initializeApp() {
   // check if the user is logged in/out, and disable inappropriate button
   if (liff.isLoggedIn()) {
     document.getElementById("liffLoginButton").disabled = true;
-    document.getElementById("login").classList.toggle("hidden");
+    document.getElementById("loadingApp").classList.toggle("hidden");
     document.getElementById("content").classList.remove("hidden");
     document.getElementById("navbar").classList.remove("hidden");
   } else {
+    document.getElementById("loadingApp").classList.toggle("hidden");
+    document.getElementById("login").classList.remove("hidden");
     document.getElementById("liffLogoutButton").disabled = true;
   }
   
